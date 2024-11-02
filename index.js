e44ffbf222ffdb24d4755951982f661f90c250e8
anegri93 <anegri93@gmail.com>
2024-11-01 21:00:50 -0300

    feat: create mongodb connection

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -5,9 +7,22 @@ const Contact = require("./routes/Contact");
 
 const app = express();
 
+// Conexión a MongoDB
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    console.log("Conectado a MongoDB");
+}).catch((error) => {
+    console.error("Error al conectar a MongoDB:", error);
+});
+
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.use("/api", Contact);
+
+// Iniciar el servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
