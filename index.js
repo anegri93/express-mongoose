162e287c4179d97cc384eb0dd903ae8065cdccdf
anegri93 <anegri93@gmail.com>
2024-11-04 16:29:58 -0300

    feat: port from env

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/api", Contact);
 
 // Iniciar el servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
--- models/contact.js
+++ models/contact.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const contactScheme = new mongoose.Schema({
     firstname: {
         type: String,
-        require: [true,"Este campo es Requerido."],
+        required: [true,"Este campo es Requerido."],
         minLength: 3,
         maxLength: 20,
         trim: true,
-        validate:{
+        validate:{  
             validator: (value) => {
                 const nameRegex = /^[A-Za-z]+$/;
                 return nameRegex.test(value);
@@ -17,19 +17,42 @@ const contactScheme = new mongoose.Schema({
     },
     lastName: {
         type: String,
-        require: [true,"Este campo es Requerido."]
+        required: [true,"Este campo es Requerido."],
+        validate:{
+            validator: (value) => {
+                const nameRegex = /^[A-Za-z]+$/;
+                return nameRegex.test(value);
+            },
+            message: "Ingresar solamente letras."
+        }
     },
     emailAddress: {
         type: String,
-        require: [true,"Este campo es Requerido."],
-        unique: true
+        required: [true,"Este campo es Requerido."],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        //Validar Mail
+        validate:{
+            validator: (value) => {
+                const emailRegex = /\S+@\S+\.\S+/;
+                return emailRegex.test(value);
+            },
+            message: "Ingresar un correo válido."
+        }
     },
     age:{
         type: Number,
-        require: false
+        require: false,
         //validar Numero
+        validate:{
+            validator: (value) => {
+                return Number.isInteger(value);
+            },
+            message: "Ingresar un número entero."
+        }
     }
 
-})
+});
 
 module.exports = mongoose.model("Contact", contactScheme);
